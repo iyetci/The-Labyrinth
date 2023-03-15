8b2033bbf1cea27ad5519d7e2b4cc3dbf553a864
yusufetci <57573152+iyetci@users.noreply.github.com>
2023-03-15 11:19:36 +0300

    Labyrinht
    Üniversite Projem

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Vector3 TeleportPosition;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<SC_FPSController>().enabled = false;
+            while (other.transform.position != TeleportPosition) 
+                other.transform.position = TeleportPosition;
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<SC_FPSController>().enabled = true;
+         
+        }
+    }
+
+}
+              
+       
+   
